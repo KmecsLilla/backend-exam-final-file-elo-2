044a1e79deee5971f24c75c42eb6ab68d9b1cefb
KmecsLilla <kmecs.lilla@gmail.com>
2021-03-24 17:04:06 +0100

    Implement chess open task

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,75 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+    private static class Statistics implements Comparable {
+        private String name;
+        private int points;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        public void setPoints(int points) {
+            this.points = points;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Statistics otherStatistics = (Statistics) o;
+            int difference = this.points - otherStatistics.points;
+            if (difference > 0) {
+                return -1;
+            } else if (difference < 0) {
+                return 1;
+            } else return 0;
+        }
     }
 
+    public static void main(String[] args) {
+        ChessResults chessResults = new ChessResults();
+        List<String> names = chessResults.getCompetitorsNamesFromFile("src/main/resources/results.txt");
+        System.out.println(names);
+    }
+
+    public List<String> getCompetitorsNamesFromFile(String fileName) {
+        List<String> lines;
+        try {
+            File file = new File(fileName);
+            lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            System.out.println("File not found!");
+            return null;
+        }
+
+        List<Statistics> statisticsList = new ArrayList<>();
+        for (String line : lines) {
+            String[] lineParts = line.split(",");
+            Statistics statistics = new Statistics();
+            statistics.setName(lineParts[0]);
+            int sum = 0;
+            for (int i = 1; i < lineParts.length; i++) {
+                sum += Integer.parseInt(lineParts[i]);
+            }
+            statistics.setPoints(sum);
+            statisticsList.add(statistics);
+        }
+        Collections.sort(statisticsList);
+        List<String> result = new ArrayList<>();
+        for (Statistics element : statisticsList) {
+            result.add(element.getName());
+        }
+        return result;
+    }
 }
